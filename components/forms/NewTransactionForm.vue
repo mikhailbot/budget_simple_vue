643a3c9316ce35ff4e12b646c5dbccf956ecb737
Mikhail Delport <mikhail@delport.ca>
2018-04-15 14:08:28 -0400

    Cleanup add transaction form

--- components/forms/NewTransactionForm.vue
+++ components/forms/NewTransactionForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full h-full max-w-sm m-auto p-6 flex justify-center flex-col">
+  <div class="w-full h-full max-w-xs m-auto p-6 flex justify-center items-center flex-col">
     <div class="block text-grey-darkest text-2xl font-bold mb-4">Add a new transaction</div>
     <form class="">
       <div class="mb-6">
@@ -12,11 +12,16 @@
         <label class="block text-grey-darker text-sm font-bold mb-2" for="account">
           Account
         </label>
-        <select v-model="account_id" class="bg-grey-lighter appearance-none border-2 border-grey-lighter focus:border-blue rounded w-full py-2 px-4 text-grey-darker outline-0" id="account">
-          <option v-for="account in accounts" :value="account.id" :key="account.id">
-            {{ account.name }}
-          </option>
-        </select>
+        <div class="relative">
+          <select v-model="account_id" class="bg-grey-lighter appearance-none border-2 border-grey-lighter focus:border-blue rounded w-full py-2 px-4 text-grey-darker outline-0" id="account">
+            <option v-for="account in accounts" :value="account.id" :key="account.id">
+              {{ account.name }}
+            </option>
+          </select>
+          <div class="pointer-events-none absolute pin-y pin-r flex items-center px-2 text-grey-darker">
+            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+          </div>
+        </div>
       </div>
       <div class="mb-6">
         <label class="block text-grey-darker text-sm font-bold mb-2" for="merchant">
@@ -28,21 +33,26 @@
         <label class="block text-grey-darker text-sm font-bold mb-2" for="category">
           Category
         </label>
-        <select v-model="category_id" class="bg-grey-lighter appearance-none border-2 border-grey-lighter focus:border-blue rounded w-full py-2 px-4 text-grey-darker outline-0" id="category">
-          <option v-for="category in categories" :value="category.id" :key="category.id">
-            {{ category.name }}
-          </option>
-        </select>
+        <div class="relative">
+          <select v-model="category_id" class="bg-grey-lighter appearance-none border-2 border-grey-lighter focus:border-blue rounded w-full py-2 px-4 text-grey-darker outline-0" id="category">
+            <option v-for="category in categories" :value="category.id" :key="category.id">
+              {{ category.name }}
+            </option>
+          </select>
+          <div class="pointer-events-none absolute pin-y pin-r flex items-center px-2 text-grey-darker">
+            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+          </div>
+        </div>
       </div>
       <div class="mb-6 pr-2 w-1/2 inline-block">
         <label class="block text-grey-darker text-sm font-bold mb-2" for="outflow">
-          Outflow
+          Expense
         </label>
         <currency-input v-model="outflow" :valueInCents="0" id="outflow"></currency-input>
       </div>
       <div class="mb-6 w-1/2 inline-block">
         <label class="block text-grey-darker text-sm font-bold mb-2" for="inflow">
-          Inflow
+          Income
         </label>
         <currency-input v-model="inflow" :valueInCents="0" id="inflow"></currency-input>
       </div>
