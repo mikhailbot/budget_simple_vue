a4737d3bb9802d4f3958e53fc7ef7fe500eaa48a
Mikhail Delport <mikhail@delport.ca>
2018-04-15 15:11:47 -0400

    Add login button

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div></div>
+  <div class="text-center my-3">
+    <nuxt-link to="/login" class="text-grey-darker text-xs font-bold bg-grey-light hover:bg-grey-lighter rounded border-b-4 border-grey hover:border-grey-light py-2 px-4 no-underline">Early Access Login</nuxt-link>
+  </div>
 </template>
 
 <script>
